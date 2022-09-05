8521202a68133f7de1f4cb5067448f6c301289b2
jainavsanghvi10 <jainavjain2001@gmail.com>
2022-09-05 23:56:45 +0530

    Added resume file and updated readme file

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Link } from '@material-ui/core';
 import { TextDecrypt } from '../content/TextDecrypt';
+
 import ResumePDF from './../../assets/Jainav_Sanghvi_Resume.pdf';
 import {
   ResumeIcon
